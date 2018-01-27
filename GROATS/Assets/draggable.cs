cddf5dd4a230623ef1e013001315358c08904bae
Amanda Rado <amandarado@amandas-macbook.attlocal.net>
2018-01-27 10:40:08 -0500

    canvas/UI as board, red/blue prefabs

---
+++ GROATS/Assets/draggable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public void OnBeginDrag(PointerEventData eventData){
+		Debug.Log ("OnBeginDrag");
+
+		//this.transform.SetParent( this.transform.parent.parent );
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log ("OnDrag");
+
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("OnEndDrag");
+
+	}	
+}
