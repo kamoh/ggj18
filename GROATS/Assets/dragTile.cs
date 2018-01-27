95c0f5c08b70d36990bc4227683f4c4d4a4d249b
kamoh <ben.serviss@gmail.com>
2018-01-26 22:57:04 -0500

    update grid with gutters, add dragTile script

---
+++ GROATS/Assets/dragTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragTile : MonoBehaviour {
+
+	// Use this for initialization
+//	void Start () {
+//		
+//	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void OnMouseDrag ()
+	{
+		Vector2 mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+	
+		transform.position = objPosition;
+	}﻿
+}
