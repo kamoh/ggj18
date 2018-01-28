5f999a94d0b5476a80793df45b3c21bda630d7c2
stuleelight <stuleelight@yahoo.com>
2018-01-28 12:33:56 -0500

    catch up, music player add, bg add

---
+++ GROATS/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	public AudioClip[] clips;
+	private AudioSource audioSource;
+	void Start () {
+		audioSource = FindObjectOfType<AudioSource>();
+		audioSource.loop = true;
+	}
+
+	private AudioClip GetRandomClip()
+
+	{
+		// Random range of clips played limited by how many clips(length).
+		return clips[Random.Range(0,clips.Length)];	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audioSource.isPlaying)
+		{
+			audioSource.clip = GetRandomClip();
+			audioSource.Play();
+		}
+		
+	}
+
+}
